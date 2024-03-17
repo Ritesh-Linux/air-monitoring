260ec0ab24410b7b34aecf361f8dc0682c5ad9b9
Ritesh Kumar <riteshnagwan@gmail.com>
2024-03-17 18:05:06 +0530

    second commit

--- src/header.h
+++ src/header.h
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #include <ESPAsyncWebServer.h>
 #include <WebSocketsServer.h>
+#include <ArduinoJson.h>
 
 AsyncWebServer server(80);
 WebSocketsServer webSocket(81);
@@ -9,22 +11,93 @@ WebSocketsServer webSocket(81);
 const char *ssid = "realme8266";
 const char *pass = "0123456789";
 
-int value, humidity, temperature, air;
-String data;
+int humidity, temperature, air;
+String airStatus;
+
+JsonDocument json;
+String jsonString;
+
+void flush() {
+    json.clear();
+    jsonString = "";
+}
 
 char html[] PROGMEM = R"(
-<body>
-    <h1>Senser value:</h1>
-    <h1 id='value'>0</h1>
-    <script>
-        socket = new WebSocket('ws://' + location.host + ':81')
-        socket.onopen = (e) => { console.log('[socket] socket.onopen')}
-        socket.onerror = (e) => { console.log('[socket] socket.onerror')}
-        socket.onmessage = (e) => {
-            document.getElementById('value').innerHTML = e.data
-        }
-    </script>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.tailwindcss.com"></script>
+        <style>
+            table {
+                width: 100%;
+            }
+            td {
+                padding: 6px;
+            }
+            table tr td:nth-child(1) {
+                text-align: left;
+            }
+            table tr td:nth-child(2) {
+                text-align: right;
+            }
+        </style>
+    </head>
+    <body>
+        <div
+            class="shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] w-[320px] mx-auto rounded-xl mt-4"
+        >
+            <div class="px-8 py-4">
+                <div
+                    class="font-bold text-xl mb-4 text-[rgba(8,_112,_184,_1)] text-center"
+                >
+                    Weather Details
+                </div>
+                <table>
+                    <tr>
+                        <td>Humidity :</td>
+                        <td id="humidity">54 %</td>
+                    </tr>
+                    <tr>
+                        <td>Temperature :</td>
+                        <td id="temperature">27 °C</td>
+                    </tr>
+                    <tr>
+                        <td>Air Quality :</td>
+                        <td id="air">80 ppm</td>
+                    </tr>
+                    <tr>
+                        <td>AQI Status :</td>
+                        <td id="status">Good Air</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <p id="value" class="mt-12 text-center"></p>
+        <script>
+            socket = new WebSocket('ws://' + location.host + ':81')
+            socket.onopen = (e) => {
+                console.log('[socket] socket.onopen')
+            }
+            socket.onerror = (e) => {
+                console.log('[socket] socket.onerror')
+            }
+            socket.onmessage = (e) => {
+                const dataString = e.data
+                if (dataString !== 'Connected') {
+					const data = JSON.parse(dataString)
+					console.log(data)
+                    document.getElementById('humidity').innerHTML = data.humidity
+                    document.getElementById('temperature').innerHTML = data.temperature
+                    document.getElementById('air').innerHTML = data.air
+                    document.getElementById('status').innerHTML = data.status
+                }
+                document.getElementById('value').innerHTML = e.data
+            }
+        </script>
+    </body>
+</html>
 )";
 
 void wifiSetup() {
@@ -38,12 +111,20 @@ void wifiSetup() {
     Serial.println("IP Address: " + WiFi.localIP().toString());
 }
 
+void mdnsSetup() {
+    Serial.println(
+        !MDNS.begin("esp")
+        ? "Error setting up mDNS responder!"
+        : "mDNS responder started"
+    );
+}
+
 void serverSetup() {
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request -> send(200, "text/html", html);
     });
     server.on("/api/monitoring", HTTP_GET, [](AsyncWebServerRequest *request){
-        request -> send(200, "application/json", "Hello, World");
+        request -> send(200, "application/json", jsonString);
     });
     server.onNotFound([](AsyncWebServerRequest *request) {
         request -> send(404, "text/plain", "Not found");
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,54 @@
 #include <header.h>
+#include <DHTesp.h>
+#include <MQ135.h>
 
 #define ledPin LED_BUILTIN
+#define BUZZERPIN D3
+#define MQ135PIN A0
+#define DHTPIN D1
+#define RZERO 494.63
+
+MQ135 mq135_sensor(MQ135PIN);
+DHTesp dht;
 
 void setup() {
     Serial.begin(115200);
+    dht.setup(DHTPIN, DHTesp::DHT11);
+    delay(1000);
     pinMode(ledPin, OUTPUT);
+    pinMode(BUZZERPIN, OUTPUT);
     wifiSetup();
     serverSetup();
     websocketSetup();
+    mdnsSetup();
 }
 
 unsigned long previousMillis;
 unsigned long currentMillis;
 
 void loop() {
+    MDNS.update();
     webSocket.loop();
-    value = analogRead(A0);
-
     currentMillis = millis();
-    if (currentMillis - previousMillis >= 1000) {
-        previousMillis = currentMillis;
+    if (currentMillis - previousMillis >= 1000) { previousMillis = currentMillis;
         digitalWrite(ledPin, !digitalRead(ledPin));
-        data = (String) value;
-        webSocket.broadcastTXT(data);
+        humidity = dht.getHumidity();
+        temperature = dht.getTemperature();
+        if (isnan(humidity) || isnan(temperature)) {
+            Serial.println("Failed to read from DHT sensor!");
+            return;
+        }
+        air = mq135_sensor.getPPM();
+        airStatus = (air <= 1000) ? "Good Air" : ((air > 1000 && air <= 2000) ? "Bad Air" : "Danger!");
+        digitalWrite(BUZZERPIN, air > 2000);
+        flush();
+        json["humidity"] = (String)humidity + " %";
+        json["temperature"] = (String) temperature + " °C";
+        json["air"] = (String) air + " ppm";
+        json["status"] = airStatus;
+        serializeJson(json, jsonString);
+//        Serial.println(jsonString);
+        webSocket.broadcastTXT(jsonString);
     }
     delay(50);
 }
